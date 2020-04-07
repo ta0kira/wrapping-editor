49b24134ba6d5bdc5121a64e984934fbf097e30b
Kevin P. Barry <ta0kira@gmail.com>
2020-04-07 13:28:47 -0700

    Adds classes for line wrapping and rendering.

--- working.hs
+++ working.hs
@@ -1,30 +1,51 @@
+{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE Safe #-}
 
+import Data.List (intercalate)
+
+
+class RenderedLines a l | a -> l where
+  getAllLines :: a -> [l]
+
 
 data CursorMove = CursorUp | CursorDown | CursorPrev | CursorNext deriving (Show)
 
 data EditAction c = InsertBefore c | InsertAfter c | DeleteBefore | DeleteAfter deriving (Show)
-
-data ViewChange u = AbsoluteFrame u | RelativeFrame u | UpdateOffset u deriving (Show)
 
 class WrappingEditor a c | a -> c where
   moveCursor :: a -> CursorMove -> a
   editData :: a -> EditAction c -> a
   getAllContent :: a -> [c]
 
-class WrappingEditor a c => ViewableEditor a c u | a -> c u where
+
+data ViewChange u = AbsoluteFrame u | RelativeFrame u | UpdateOffset u deriving (Show)
+
+class LineViewport a u | a -> u where
   changeView :: a -> ViewChange u -> a
   seekInView :: a -> u -> a
-  getVisible :: a -> [[c]]
   getCursor :: a -> Maybe u
 
-splitAllLines :: ([c] -> (l,[c])) -> [c] -> [l]
-splitAllLines f [] = []
-splitAllLines f cs = let (l,cs2) = f cs in l:(splitAllLines f cs2)
+
+-- NOTE: Using this instead of [c] allows for something like a line-break type
+-- (e.g., hyphen, ellipsis) to be included later on.
+data LineViewer c =
+  LineViewer {
+    lvLine :: [c]
+  }
+  deriving (Show)
+
+newLineViewer :: [c] -> LineViewer c
+newLineViewer cs = editor where
+  editor = LineViewer {
+      lvLine = cs
+    }
+
+instance RenderedLines (LineViewer c) (LineViewer c) where
+  getAllLines = (:[])
 
 
 data LineEditor c =
@@ -60,20 +81,8 @@ getFullLine e = LineViewer $ reverse (leCharsBefore e) ++ leCharsAfter e
 getLineSize :: LineEditor c -> Int
 getLineSize = leSize
 
-
--- NOTE: Using this instead of [c] allows for something like a line-break type
--- (e.g., hyphen, ellipsis) to be included later on.
-data LineViewer c =
-  LineViewer {
-    lvLine :: [c]
-  }
-  deriving (Show)
-
-newLineViewer :: [c] -> LineViewer c
-newLineViewer cs = editor where
-  editor = LineViewer {
-      lvLine = cs
-    }
+instance RenderedLines (LineEditor c) (LineViewer c) where
+  getAllLines = getAllLines . getFullLine
 
 
 data UnparsedPara c =
@@ -111,6 +120,12 @@ seekParaLine n (ParaEditor bs l as p) = seek bs (getFullLine l) as p where
     | p > n && not (null bs) = seek (tail bs) (head bs) (l:as) (p-1)
     | otherwise = ParaEditor bs (newLineEditor l) as p
 
+instance RenderedLines (ParaEditor c) (LineViewer c) where
+  getAllLines (ParaEditor bs l as p) =
+    concat (reverse $ map getAllLines bs) ++
+    getAllLines l ++
+    concat (map getAllLines as)
+
 
 data ParaViewer c =
   ParaViewer {
@@ -124,15 +139,8 @@ newParaViewer wrap (UnparsedPara cs) = editor where
       pvLines = splitAllLines wrap cs
     }
 
-
-data ViewBuffer c =
-  ViewBuffer {
-    vbLinesBefore :: [[c]],  -- Reversed.
-    vbLinesCurrent :: [[c]],
-    vbLinesAfter :: [[c]]
-  } |
-  EmptyViewBuffer
-  deriving (Show)
+instance RenderedLines (ParaViewer c) (LineViewer c) where
+  getAllLines = concat . map getAllLines . pvLines
 
 
 data FixedCharSize =
@@ -143,34 +151,67 @@ data FixedCharSize =
   deriving (Show)
 
 
-data TextEditor c =
-  TextEditor {
-    teHiddenBefore :: [UnparsedPara c],  -- Reversed.
-    teVisibleBefore :: [ParaViewer c],  -- Reversed.
-    teEditing :: ParaEditor c,
-    teVisibleAfter :: [ParaViewer c],
-    teHiddenAfter :: [UnparsedPara c]
-  }
-  deriving (Show)
+class Show a => FixedLineBreaker a c where
+  breakLine :: a -> Int -> [c] -> (LineViewer c,[c])
+  joinLines :: a -> [LineViewer c] -> [c]
 
-newTextEditor :: ([c] -> (LineViewer c,[c])) -> [UnparsedPara c] -> TextEditor c
-newTextEditor wrap [] = newTextEditor wrap [UnparsedPara []]
-newTextEditor wrap (p:ps) =
-  TextEditor {
-    teHiddenBefore = [],
-    teVisibleBefore = [],
-    teEditing = newParaEditor wrap p,
-    teVisibleAfter = [],
-    teHiddenAfter = ps
+
+data TextEditor c =
+  forall a. FixedLineBreaker a c => TextEditor {
+    teBefore :: [ParaViewer c],  -- Reversed.
+    teEditing :: ParaEditor c,
+    teAfter :: [ParaViewer c],
+    teViewSize :: FixedCharSize,
+    teViewOffset :: FixedCharSize,
+    teWrap :: a
   }
+
+
+instance Show c => Show (TextEditor c) where
+  show (TextEditor bs e as s o w) =
+    (concat $ map (++ "\n") $ map show $ reverse bs) ++
+    show e ++ "\n" ++
+    (concat $ map (++ "\n") $ map show as) ++
+    "Size: " ++ show s ++ ", Corner: " ++ show o ++ ", Wrap: " ++ show w
+
+newTextEditor :: FixedLineBreaker a c => FixedCharSize -> a -> [UnparsedPara c] -> TextEditor c
+newTextEditor s wrap [] = newTextEditor s wrap [UnparsedPara []]
+newTextEditor s@(FixedCharSize w h) wrap (p:ps) =
+  TextEditor {
+    teBefore = [],
+    teEditing = newParaEditor (breakLine wrap w) p,
+    teAfter = map (newParaViewer (breakLine wrap w)) ps,
+    teViewSize = s,
+    teViewOffset = FixedCharSize 0 0,
+    teWrap = wrap
+  }
+
+flattenEditorContents :: TextEditor c -> [UnparsedPara c]
+flattenEditorContents (TextEditor bs e as _ _ w) =
+  (map (flattenToPara w) $ reverse bs) ++
+  [flattenToPara w e] ++
+  (map (flattenToPara w) as)
+
+
+splitAllLines :: ([c] -> (l,[c])) -> [c] -> [l]
+splitAllLines f [] = []
+splitAllLines f cs = let (l,cs2) = f cs in l:(splitAllLines f cs2)
+
+flattenToPara :: (FixedLineBreaker a c, RenderedLines b (LineViewer c)) => a -> b -> UnparsedPara c
+flattenToPara w = UnparsedPara . joinLines w . getAllLines
 
 
 breakParagraphs = map UnparsedPara . lines
 
-fixedLineBreaks n xs = (LineViewer $ take n xs,drop n xs)
+data LineBreakPolicy = FixedLineWidth deriving Show
+
+instance FixedLineBreaker LineBreakPolicy Char where
+  breakLine FixedLineWidth n cs = (LineViewer $ take n cs,drop n cs)
+  joinLines _ = concat . map lvLine
 
 main = do
   contents <- readFile "testdata.txt"
   let paras = breakParagraphs contents
-  let editor = newTextEditor (fixedLineBreaks 5) paras
+  let editor = newTextEditor (FixedCharSize 5 10) FixedLineWidth paras
   putStrLn $ show editor
+  putStrLn $ show $ flattenEditorContents editor
