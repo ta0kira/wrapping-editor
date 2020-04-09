6767cd05ac569356c91d300876c310912ed92727
Kevin P. Barry <ta0kira@gmail.com>
2020-04-08 17:24:14 -0700

    Makes a mess of the original working file and adds a new working file.

--- working.hs
+++ working.hs
@@ -9,28 +9,27 @@ import Data.List (intercalate)
 
 
 class RenderedLines a l | a -> l where
-  getAllLines :: a -> [l]
+  getVisibleLines :: a -> [l]
 
 
-data CursorMove = CursorUp | CursorDown | CursorPrev | CursorNext deriving (Show)
+data CursorMove = CursorUp | CursorDown | CursorPrev | CursorNext deriving (Eq,Show)
 
-data EditAction c = InsertBefore c | InsertAfter c | DeleteBefore | DeleteAfter deriving (Show)
+data CursorStatus = CursorAtFront | CursorAtTop | CursorInMiddle | CursorAtBottom | CursorAtBack deriving (Eq,Show)
 
-class WrappingEditor a c | a -> c where
+data EditDirection = EditBefore | EditAfter deriving (Eq,Show)
+
+data EditAction c = EditInsert c | EditDelete deriving (Show)
+
+class LineBasedEditor a c u | a -> c u where
   moveCursor :: a -> CursorMove -> a
-  editData :: a -> EditAction c -> a
-  getAllContent :: a -> [c]
+  getCursorPos :: a -> u
+  getCursorStatus :: a -> CursorStatus
+  editData :: a -> EditAction c -> EditDirection -> a
+  breakPara :: a -> EditDirection -> a
+  getFlatDocument :: a -> [c]
 
 
-data ViewChange u = AbsoluteFrame u | RelativeFrame u | UpdateOffset u deriving (Show)
-
-class LineViewport a u | a -> u where
-  changeView :: a -> ViewChange u -> a
-  seekInView :: a -> u -> a
-  getCursor :: a -> Maybe u
-
-
-data LineBreak = SymbolBreak | WhitespaceBreak | BrokenWord | ParaBreak deriving (Show)
+data LineBreak = SymbolBreak | WhitespaceBreak | BrokenWord | ParaBreak deriving (Eq,Show)
 
 data LineViewer c =
   LineViewer {
@@ -50,7 +49,7 @@ emptyLineViewer :: LineViewer c
 emptyLineViewer = newLineViewer [] ParaBreak
 
 instance RenderedLines (LineViewer c) (LineViewer c) where
-  getAllLines = (:[])
+  getVisibleLines = (:[])
 
 
 data LineEditor c =
@@ -73,23 +72,57 @@ newLineEditor (LineViewer cs b) =
     leSize = length cs
   }
 
-getLineChar :: LineEditor c -> Int
-getLineChar = lePosition
-
-seekLineChar :: Int -> LineEditor c -> LineEditor c
-seekLineChar n e@(LineEditor bs as b p s)
-  | p < n && not (null as) = seekLineChar n (LineEditor (head as:bs) (tail as) b (p+1) s)
-  | p > n && not (null bs) = seekLineChar n (LineEditor (tail bs) (head bs:as) b (p-1) s)
-  | otherwise = e
-
 getFullLine :: LineEditor c -> LineViewer c
 getFullLine (LineEditor bs as b _ _) = LineViewer (reverse bs ++ as) b
 
-getLineSize :: LineEditor c -> Int
-getLineSize = leSize
+-- TODO:
+seekLineChar :: Int -> LineEditor c -> LineEditor c
+seekLineChar k2 e@(LineEditor bs as b k n)
+  | k2 < k && not (null as) = seekLineChar n (LineEditor (head as:bs) (tail as) b (k+1) n)
+  | k2 > n && not (null bs) = seekLineChar n (LineEditor (tail bs) (head bs:as) b (k-1) n)
+  -- TODO: This allows the cursor to be past the end. Make sure it's bounded by
+  -- n when move/edit operations happen.
+  | otherwise = LineEditor bs as b k2 n
+
+moveLineCursorPrev :: LineEditor c -> LineEditor c
+moveLineCursorPrev e@(LineEditor bs as b k n)
+  | null bs = e
+  | otherwise = LineEditor (tail bs) (head bs:as) b (k-1) n
+
+moveLineCursorNext :: LineEditor c -> LineEditor c
+moveLineCursorNext e@(LineEditor bs as b k n)
+  | null as = e
+  | otherwise = LineEditor (head as:bs) (tail as) b (k+1) n
+
+getLineCursorStatus :: LineEditor c -> CursorStatus
+getLineCursorStatus e@(LineEditor as bs _ k n)
+  | k == 0 || null bs = CursorAtFront
+  | k >= n || null as = CursorAtBack
+  | otherwise = CursorInMiddle
+
+editLineData :: EditAction c -> EditDirection -> LineEditor c -> LineEditor c
+editLineData (EditInsert x) d e@(LineEditor bs as b k n)
+  | d == EditBefore = LineEditor (x:bs) as    b (k+1) (n+1)
+  | d == EditAfter  = LineEditor bs    (x:as) b k     (n+1)
+editLineData EditDelete d e@(LineEditor bs as b k n)
+  | getLineCursorStatus e /= CursorAtTop    && d == EditBefore = LineEditor (tail bs) as        b (k-1) (n-1)
+  | getLineCursorStatus e /= CursorAtBottom && d == EditAfter  = LineEditor bs        (tail as) b (k-1) (n-1)
+  | otherwise = e
+
+breakLineAtCursor :: LineEditor c -> (LineViewer c, LineViewer c)
+breakLineAtCursor (LineEditor bs as b _ _) = (newLineViewer (reverse bs) ParaBreak,newLineViewer as b)
+
+mergeEditedLines :: Maybe (LineViewer c) -> LineEditor c -> Maybe (LineViewer c) -> LineEditor c
+mergeEditedLines b l a = addBefore b $ addAfter a l where
+  addBefore (Just (LineViewer cs _)) (LineEditor bs as b k n) =
+    LineEditor (reverse cs ++ bs) as b (k + length cs) (n + length cs)
+  addBefore _ l = l
+  addAfter (Just (LineViewer cs b)) (LineEditor bs as _ k n) =
+    LineEditor bs (as ++ cs) b k (n + length cs)
+  addAfter _ l = l
 
 instance RenderedLines (LineEditor c) (LineViewer c) where
-  getAllLines = getAllLines . getFullLine
+  getVisibleLines = (:[]) . getFullLine
 
 
 data UnparsedPara c =
@@ -104,12 +137,21 @@ splitAllLines _ [] = []
 splitAllLines f cs = let (l,cs2) = f cs in l:(splitAllLines f cs2)
 
 
+data FixedCharSize =
+  FixedCharSize {
+    fcsHorizontal :: Int,
+    fcsVertical :: Int
+  }
+  deriving (Eq,Show)
+
+
 data ParaEditor c =
   ParaEditor {
     peLinesBefore :: [LineViewer c],  -- Reversed.
     peLineCurrent :: LineEditor c,
     peLinesAfter :: [LineViewer c],
-    pePosition :: Int
+    peCursorLine :: Int,
+    peTotalLines :: Int
   }
   deriving (Show)
 
@@ -119,24 +161,77 @@ newParaEditor wrap (UnparsedPara cs) = editor where
       peLinesBefore = [],
       peLineCurrent = newLineEditor first,
       peLinesAfter = rest,
-      pePosition = 0
+      peCursorLine = 0,
+      peTotalLines = length rest + 1
     }
   (first:rest) = nonempty (splitAllLines wrap cs)
   nonempty [] = [emptyLineViewer]
   nonempty ls = ls
 
-seekParaLine :: Int -> ParaEditor c -> ParaEditor c
-seekParaLine n (ParaEditor bs l as p) = seek bs (getFullLine l) as p where
-  seek bs l as p
-    | p < n && not (null as) = seek (l:bs) (head as) (tail as) (p+1)
-    | p > n && not (null bs) = seek (tail bs) (head bs) (l:as) (p-1)
-    | otherwise = ParaEditor bs (newLineEditor l) as p
+-- TODO: Cursor up/down only works properly with fixed-width viewing => should
+-- ParaEditor and LineEditor be tied to FixedCharSize?
+-- TODO: Cursor up/down doesn't preserve end-of-line when moving from long to
+-- short to long lines, e.g., going from 10 to 5 to 10 leaves the cursor at 5.
+moveParaCursor :: CursorMove -> ParaEditor c -> ParaEditor c
+moveParaCursor CursorUp e@(ParaEditor bs l as k n)
+  | k <= 0 || null bs = ParaEditor [] l as 0 n  -- Corrects k and bs, just in case.
+  | otherwise = ParaEditor (tail bs) editor (getFullLine l:as) (k-1) n where
+    editor = seekLineChar (lePosition l) $ newLineEditor (head bs)
+moveParaCursor CursorDown e@(ParaEditor bs l as k n)
+  | k >= n || null as = ParaEditor bs l [] 0 n  -- Corrects k and as, just in case.
+  | otherwise = ParaEditor (getFullLine l:bs) editor (tail as) (k+1) n where
+    editor = seekLineChar (lePosition l) $ newLineEditor (head as)
+moveParaCursor CursorPrev e@(ParaEditor bs l as k n)
+  | getLineCursorStatus l /= CursorAtTop = (ParaEditor bs (moveLineCursorPrev l) as k n)
+  | otherwise = ParaEditor (tail bs) editor' (getFullLine l:as) (k-1) n where
+    editor = newLineEditor (head bs)
+    editor' = seekLineChar (leSize editor) editor  -- Seek the end of the line.
+moveParaCursor CursorNext e@(ParaEditor bs l as k n)
+  | getLineCursorStatus l /= CursorAtBottom = (ParaEditor bs (moveLineCursorNext l) as k n)
+  | otherwise = ParaEditor (getFullLine l:bs) editor (tail as) (k+1) n where
+    editor = newLineEditor (head as)
+
+getParaCursorStatus :: ParaEditor c -> CursorStatus
+getParaCursorStatus (ParaEditor _ l _ k n)
+  | k == 0 = if getLineCursorStatus l == CursorAtFront
+                then CursorAtFront
+                else CursorAtTop
+  | k == n = if getLineCursorStatus l == CursorAtBack
+                then CursorAtBack
+                else CursorAtBottom
+  | otherwise = CursorInMiddle
+
+editParaData :: ([LineViewer c] -> [c]) ->  ([c] -> (LineViewer c,[c])) -> EditAction c -> EditDirection -> ParaEditor c -> ParaEditor c
+editParaData join wrap i d (ParaEditor bs l as k n) = process i where
+  process (EditInsert _) = reparse $ ParaEditor bs (editLineData i d l) as k n
+  process EditDelete = reparse editor where
+    (b,bs2) = takeHead bs
+    (a,as2) = takeHead as
+    l2 = mergeEditedLines a l b
+    k2 = k - (if null bs then 0 else 1)
+    n2 = n - (if null bs then 0 else 1) - (if null as then 0 else 1)
+    editor = (ParaEditor bs2 (editLineData i d l2) as2 k2 n2)
+    takeHead cs = if null cs
+                     then (Nothing,cs)
+                     else (Just $ head cs,tail cs)
+  reparse (ParaEditor bs l as k n) = moveBy position editor where
+    position = lePosition l
+    editor = ParaEditor bs (newLineEditor first) rest k (length rest + k + 1)
+    (first:rest) = splitAllLines wrap $ join (getFullLine l:as)
+    moveBy k e
+      | k > 0 = moveBy (k-1) $ moveParaCursor CursorNext e
+      | otherwise = e
+
+breakParaAtCursor :: ([LineViewer c] -> [c]) -> ParaEditor c -> (UnparsedPara c, UnparsedPara c)
+breakParaAtCursor join (ParaEditor bs l as k n) =
+  (UnparsedPara $ join $ reverse $ before:bs,UnparsedPara $ join $ after:as) where
+    (before,after) = breakLineAtCursor l
 
 instance RenderedLines (ParaEditor c) (LineViewer c) where
-  getAllLines (ParaEditor bs l as p) =
-    concat (reverse $ map getAllLines bs) ++
-    getAllLines l ++
-    concat (map getAllLines as)
+  getVisibleLines (ParaEditor bs l as _ _) =
+    concat (reverse $ map getVisibleLines bs) ++
+    getVisibleLines l ++
+    concat (map getVisibleLines as)
 
 
 data ParaViewer c =
@@ -152,23 +247,16 @@ newParaViewer wrap (UnparsedPara cs) = editor where
     }
 
 instance RenderedLines (ParaViewer c) (LineViewer c) where
-  getAllLines = nonempty . concat . map getAllLines . pvLines where
+  getVisibleLines = nonempty . concat . map getVisibleLines . pvLines where
     nonempty [] = [emptyLineViewer]
     nonempty ls = ls
-
-
-data FixedCharSize =
-  FixedCharSize {
-    fcsWidth :: Int,
-    fcsHeight :: Int
-  }
-  deriving (Show)
 
 
 class Show a => FixedLineBreaker a c where
   breakLine :: a -> Int -> [c] -> (LineViewer c,[c])
   renderLine :: a -> LineViewer c -> [c]
   joinLines :: a -> [LineViewer c] -> [c]
+  joinParas :: a -> [UnparsedPara c] -> [c]
 
 
 data ViewBuffer c =
@@ -206,17 +294,16 @@ data TextEditor c =
     teAfter :: [UnparsedPara c],
     teBuffer :: ViewBuffer c,
     teViewSize :: FixedCharSize,
-    teViewOffset :: FixedCharSize,
     teEditCursor :: FixedCharSize,  -- Relative to top-left of view.
     teWrap :: a
   }
 
 instance Show c => Show (TextEditor c) where
-  show (TextEditor bs e as _ s o c w) =
+  show (TextEditor bs e as _ s c w) =
     (concat $ map (++ "\n") $ map show $ reverse bs) ++
     show e ++ "\n" ++
     (concat $ map (++ "\n") $ map show as) ++
-    "Size: " ++ show s ++ ", Corner: " ++ show o ++ ", Cursor: " ++ show c ++ ", Wrap: " ++ show w
+    "Size: " ++ show s ++ ", Cursor: " ++ show c ++ ", Wrap: " ++ show w
 
 newTextEditor :: FixedLineBreaker a c => FixedCharSize -> a -> [UnparsedPara c] -> TextEditor c
 newTextEditor s wrap [] = newTextEditor s wrap [UnparsedPara []]
@@ -227,26 +314,41 @@ newTextEditor s@(FixedCharSize w h) wrap (p:ps) =
     teAfter = ps,
     teBuffer = buffer,
     teViewSize = s,
-    teViewOffset = FixedCharSize 0 0,
     teEditCursor = FixedCharSize 0 0,
     teWrap = wrap
   } where
     editing = newParaEditor (breakLine wrap w) p
     rendered = map (newParaViewer (breakLine wrap w)) ps
-    buffer = newViewBuffer [] (getAllLines editing ++ concat (map getAllLines rendered))
+    buffer = newViewBuffer [] (getVisibleLines editing ++ concat (map getVisibleLines rendered))
 
-renderEditorContents :: TextEditor c -> [[c]]
-renderEditorContents (TextEditor _ _ _ b (FixedCharSize _ h) _ (FixedCharSize _ cy) w) = contents where
-  contents = map (renderLine w) $ getViewLines na nb b
-  na = cy
-  nb = h-cy
+instance RenderedLines (TextEditor c) [c] where
+  getVisibleLines (TextEditor _ _ _ b (FixedCharSize _ vy) (FixedCharSize _ cy) w) = contents where
+    contents = map (renderLine w) $ getViewLines na nb b
+    na = cy
+    nb = vy-cy
 
-flattenEditorContents :: TextEditor c -> [UnparsedPara c]
-flattenEditorContents (TextEditor bs e as _ _ _ _ w) = reverse bs ++ [flattenToPara w e] ++ as
+instance LineBasedEditor (TextEditor c) c FixedCharSize where
+  moveCursor ea@(TextEditor bs e as b s c w) m = process (getParaCursorStatus e) m where
+    process status CursorUp
+      | status == CursorAtTop || status == CursorAtFront = ea  -- TODO: Move to same pos in last line of prev para.
+    process status CursorDown
+      | status == CursorAtBottom || status == CursorAtBack = ea  -- TODO: Move to same pos in first line of next para.
+    process CursorAtFront CursorPrev = ea
+    process CursorAtBack CursorNext = ea
+    -- TODO: Update cursor position.
+    process _ _ = TextEditor bs (moveParaCursor m e) as (shiftView m b) s c w where
+    shiftView CursorUp   = shiftViewFocus (-1)
+    shiftView CursorDown = shiftViewFocus 1
+    shiftView _          = id
+--   getCursorPos :: a -> u
+--   getCursorStatus :: a -> CursorStatus
+--   editData :: a -> EditAction c -> EditDirection -> a
+--   breakPara :: a -> EditDirection -> a
+--   getFlatDocument :: a -> [c]
 
 
 flattenToPara :: (FixedLineBreaker a c, RenderedLines b (LineViewer c)) => a -> b -> UnparsedPara c
-flattenToPara w = UnparsedPara . joinLines w . getAllLines
+flattenToPara w = UnparsedPara . joinLines w . getVisibleLines
 
 
 -- Testing stuff -->
@@ -276,11 +378,12 @@ instance FixedLineBreaker LineBreakPolicy Char where
   renderLine _ (LineViewer cs BrokenWord) = cs ++ "-"
   renderLine _ (LineViewer cs _)          = cs
   joinLines _ = concat . map unrenderLine
+  joinParas _ = concat . map (++ "\n") . map upData
 
 main = do
   contents <- readFile "testdata.txt"
   let paras = breakParagraphs contents
   let editor = newTextEditor (FixedCharSize 20 10) FixedLineWidth paras
   putStrLn $ show editor
-  putStr $ unlines $ map upData $ flattenEditorContents editor
-  putStr $ unlines $ renderEditorContents editor
+--   putStr $ getAllContent editor
+  putStr $ unlines $ getVisibleLines editor
---
+++ working2.hs
@@ -0,0 +1,191 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE Safe #-}
+
+
+-- Viewers.
+
+data LineBreakType = ParaBreak | SpaceBreak | BrokenWord | TokenBreak deriving (Eq,Ord,Show)
+
+data VisibleLine c =
+  VisibleLine {
+    vlText :: [c],
+    vlWidth :: Int,
+    vlBreak :: LineBreakType
+  }
+  deriving (Show)
+
+data StaticPara c =
+  UnparsedPara {
+    upText :: [c]
+  } |
+  VisibleParaBefore {
+    vpaLines :: [VisibleLine c]  -- Reversed.
+  } |
+  VisibleParaAfter {
+    vpbLines :: [VisibleLine c]
+  }
+  deriving (Show)
+
+
+-- Editors.
+
+data EditAction c = InsertText [c] | DeleteText Int deriving (Show)
+
+data EditDirection = EditBefore | EditAfter deriving (Eq,Ord,Show)
+
+data MoveDirection = MoveUp | MoveDown | MovePrev | MoveNext deriving (Eq,Ord,Show)
+
+data EditingLine c =
+  EditingLine {
+    elTextBefore :: [c],  -- Reversed.
+    elTextAfter :: [c],
+    elCursor :: Int,
+    elWidth :: Int,
+    elBreak :: LineBreakType
+  }
+  deriving (Show)
+
+data EditingPara c =
+  EditingPara {
+    epBefore :: [VisibleLine c],  -- Reversed.
+    epEditing :: EditingLine c,
+    epAfter :: [VisibleLine c],
+    epLine :: Int,
+    epHeight :: Int
+  }
+  deriving (Show)
+
+data EditingDocument c =
+  EditingDocument {
+    edBefore :: [StaticPara c],  -- Reversed.
+    edEditing :: EditingPara c,
+    edAfter :: [StaticPara c],
+    edWidth :: Int,
+    edHeight :: Int,
+    edViewOffset :: Int
+  }
+  deriving (Show)
+
+
+-- Parsing.
+
+class FixedFontParser a c | a -> c where
+  setLineWidth :: a -> Int -> a
+  breakParas :: a -> [c] -> [StaticPara c]
+  joinParas :: a -> [StaticPara c] -> [c]
+  breakLines :: a -> [c] -> [VisibleLine c]
+  joinLines :: a -> [VisibleLine c] -> [c]
+  renderLine :: a -> VisibleLine c -> [c]
+
+
+-- Line Actions.
+
+emptyLine :: VisibleLine c
+emptyLine = VisibleLine [] 0 ParaBreak
+
+editLine :: VisibleLine c -> EditingLine c
+editLine (VisibleLine cs w b) = EditingLine [] cs 0 w b
+
+viewLine :: EditingLine c -> VisibleLine c
+viewLine (EditingLine bs as _ w b) = VisibleLine (reverse bs ++ as) w b
+
+getLineCursor :: EditingLine c -> Int
+getLineCursor = elCursor
+
+setLineCursor :: Int -> EditingLine c -> EditingLine c
+setLineCursor k e@(EditingLine bs as c w b)
+  | k < c && not (null bs) = setLineCursor k $ EditingLine (tail bs) (head bs:as) (c-1) w b
+  | k > c && not (null as) = setLineCursor k $ EditingLine (head as:bs) (tail as) (c+1) w b
+  | otherwise = e
+
+splitLine :: EditingLine c -> (VisibleLine c,VisibleLine c)
+splitLine (EditingLine bs as c w b) =
+  (VisibleLine (reverse bs) (length bs) ParaBreak,
+   VisibleLine as (length as) b)
+
+lineCursorMovable :: MoveDirection -> EditingLine c -> Bool
+lineCursorMovable MovePrev (EditingLine (_:_) _ _ _ _) = True
+lineCursorMovable MoveNext (EditingLine _ (_:_) _ _ _) = True
+lineCursorMovable _ _ = False
+
+moveLineCursor :: MoveDirection -> EditingLine c -> EditingLine c
+moveLineCursor MovePrev (EditingLine bs as c w b)
+  | not (null bs) = (EditingLine (tail bs) (head bs:as) (c-1) w b)
+moveLineCursor MoveNext (EditingLine bs as c w b)
+  | not (null as) = (EditingLine (head as:bs) (tail as) (c+1) w b)
+moveLineCursor _ l = l
+
+modifyLine :: EditAction c -> EditDirection -> EditingLine c -> EditingLine c
+modifyLine (InsertText cs) d (EditingLine bs as c w b) = revised where
+  bs2 = if d == EditBefore
+           then reverse cs ++ bs
+           else bs
+  as2 = if d == EditAfter
+           then cs ++ as
+           else as
+  revised = EditingLine bs2 as2 (length bs2) (length bs2 + length as2) b
+modifyLine (DeleteText k) d (EditingLine bs as c w b) = revised where
+  bs2 = if d == EditBefore
+           then drop k bs
+           else bs
+  as2 = if d == EditAfter
+           then drop k as
+           else as
+  revised = EditingLine bs2 as2 (length bs2) (length bs2 + length as2) b
+
+
+-- Paragraph Actions.
+
+flattenPara :: FixedFontParser a c => a -> StaticPara c -> [c]
+flattenPara parser (UnparsedPara cs) = cs
+flattenPara parser (VisibleParaBefore ls) = reverse $ joinLines parser ls
+flattenPara parser (VisibleParaAfter ls) = joinLines parser ls
+
+parseParaBefore :: FixedFontParser a c => a -> StaticPara c -> StaticPara c
+parseParaBefore parser para = VisibleParaBefore (reverse $ breakLines parser cs) where
+  cs = flattenPara parser para
+
+parseParaAfter :: FixedFontParser a c => a -> StaticPara c -> StaticPara c
+parseParaAfter parser para = VisibleParaBefore (breakLines parser cs) where
+  cs = flattenPara parser para
+
+editPara :: FixedFontParser a c => a -> StaticPara c -> EditingPara c
+editPara parser para = EditingPara [] (editLine line) after 0 (length after + 1) where
+  (line:after) = nonempty $ breakLines parser $ flattenPara parser para
+  nonempty [] = [emptyLine]
+  nonempty ls = ls
+
+viewPara :: FixedFontParser a c => a -> EditingPara c -> StaticPara c
+viewPara parser (EditingPara bs l as _ _) = VisibleParaAfter ls where
+  ls = reverse bs ++ [viewLine l] ++ as
+
+getParaCursor :: EditingPara c -> (Int,Int)
+getParaCursor (EditingPara _ l _ n _) = (getLineCursor l,n)
+
+setParaCursor :: Int -> EditingPara c -> EditingPara c
+setParaCursor k e@(EditingPara bs l as n h) = (EditingPara bs (setLineCursor k l) as n h)
+
+splitPara :: EditingPara c -> (StaticPara c,StaticPara c)
+splitPara (EditingPara bs l as _ _) =
+  (VisibleParaBefore bs,VisibleParaAfter (viewLine l:as))
+
+paraCursorMovable :: MoveDirection -> EditingPara c -> Bool
+paraCursorMovable d (EditingPara bs l as _ _)
+  | d == MoveUp   = not (null bs)
+  | d == MoveDown = not (null as)
+  | otherwise = lineCursorMovable d l
+
+-- NOTE: Cursor position not preserved for up/down movements.
+moveParaCursor :: MoveDirection -> EditingPara c -> EditingPara c
+moveParaCursor d p@(EditingPara bs l as n h)
+  | not (paraCursorMovable d p) = p
+  | d == MoveUp   = EditingPara (tail bs) (editLine $ head bs) (viewLine l:as) (n-1) h
+  | d == MoveDown = EditingPara (viewLine l:bs) (editLine $ head as) (tail as) (n+1) h
+  | otherwise = EditingPara bs (moveLineCursor d l) as n h
+
+modifyPara :: FixedFontParser a c => a -> EditAction c -> EditDirection -> EditingPara c -> EditingPara c
+modifyPara parser (InsertText cs) d p@(EditingPara bs l as n h) = p  -- TODO: Implement.
+modifyPara parser (DeleteText k)  d p@(EditingPara bs l as n h) = p  -- TODO: Implement.
